cca37002bd8908ba117e543d571feb4c17600dd9
Qadree Trimble <qadree_t@yahoo.com>
2021-03-22 15:22:12 -0700

    continue testing

--- code-challenges/challenges-01.test.js
+++ code-challenges/challenges-01.test.js
@@ -25,6 +25,11 @@ Use `forEach` to loop over the input array. Modify each string, and add the upda
 ------------------------------------------------------------------------------------------------ */
 
 const addExclamation = (arr) => {
+  const strings = [];
+  arr.forEach(function(string, i) {
+    strings.push(string + !);
+  });
+  return strings 
   // Solution code here...
 };
 
